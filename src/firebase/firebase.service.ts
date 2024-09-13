80d1bff3807838d8f8928714df1565b44bd40f5a
redi <rediballa1@gmail.com>
2024-09-13 14:48:11 +0200

    dds

--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -1,7 +1,6 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import * as admin from 'firebase-admin';
 import sharp from 'sharp';
-
 @Injectable()
 export class FirebaseService {
   async uploadFile(
@@ -13,46 +12,37 @@ export class FirebaseService {
       const bucket = admin.storage().bucket(process.env.FIREBASE_BUCKETNAME);
       const fileName = `${Date.now()}_${file.mimetype.startsWith('image/') ? file.originalname.split('.').slice(0, -1).join('.') + '.webp' : file.originalname}`;
       const fileUpload = bucket.file(`${directoryToSave}/${fileName}`);
-
       let fileBuffer = file.buffer;
-
+      let contentType = file.mimetype.startsWith('image/')? 'image/webp' : file.mimetype;
       if (file.mimetype.startsWith('image/')) {
         if (aspect === 'square') {
           const metadata = await sharp(file.buffer).metadata();
-
           const size = Math.min(metadata.width, metadata.height);
-
           const left = Math.floor((metadata.width - size) / 2);
           const top = Math.floor((metadata.height - size) / 2);
-
           fileBuffer = await sharp(file.buffer)
             .extract({ width: size, height: size, left, top })
             .toBuffer();
         }
-
         fileBuffer = await sharp(fileBuffer)
           .webp({
             quality: 75,
           })
           .toBuffer();
       }
-
       const stream = fileUpload.createWriteStream({
         metadata: {
-          contentType: 'image/webp',
+          contentType:contentType,
         },
       });
-
       await new Promise<void>((resolve, reject) => {
         stream.on('error', (error) => {
           console.error('Error in stream:', error);
           reject(new ConflictException('Failed to upload file'));
         });
-
         stream.on('finish', resolve);
         stream.end(fileBuffer);
       });
-
       await fileUpload.makePublic();
       const publicUrl = fileUpload.publicUrl();
       return publicUrl;
